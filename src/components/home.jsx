f5bc48416405512346e5364423d4c6c53445d94e
rotemshaked <rotemshaked8@gmail.com>
2022-01-01 23:47:59 +0200

    1.1.2022

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,33 +5,36 @@ import "../assets/styles.css";
 import Song from "./songLyrics";
 
 const Home = () => {
-  let [artistName, setArtistName] = useState("hello world");
   let [songName, setSongName] = useState("hello world");
   let [songlyrics, setSongLyrics] = useState("hello world");
 
   let userInput = (e) => {
     let temporary = e.target.value;
-    setArtistName(temporary);
-  };
-  let userInput2 = (e) => {
-    let temporary = e.target.value;
     setSongName(temporary);
   };
+  // let userInput2 = (e) => {
+  //   let temporary = e.target.value;
+  //   setSongName(temporary);
+  // };
 
-  let searchSong = (input) => {
-    axios(`https://api.lyrics.ovh/v1/${artistName}/${songName}`).then(
-      (song) => {
+  let searchSong = () => {
+    axios(`https://api.lyrics.ovh/suggest/${songName}`).then((song) => {
+      console.log(song);
+      axios(
+        `https://api.lyrics.ovh/v1/${song.data.data[0].artist.name}/${songName}`
+      ).then((song) => {
         let songLyrics = song.data.lyrics;
         setSongLyrics(songLyrics);
+        console.log(song);
         console.log(songLyrics);
-      }
-    );
+      });
+    });
     return <div lyrics={songlyrics}></div>;
   };
 
-  const dataShow = () => {
-    return <Song lyrics={songlyrics} />;
-  };
+  // const dataShow = () => {
+  //   return <Song lyrics={songlyrics} />;
+  // };
 
   // useEffect(() => {
   //   return dataShow();
@@ -39,11 +42,11 @@ const Home = () => {
 
   return (
     <div className="homePageContainer">
-      <input onChange={userInput} placeholder="artist name"></input>
-      <input onChange={userInput2} placeholder="song name"></input>
+      <input onChange={userInput} placeholder="song name"></input>
+      {/* <input onChange={userInput2} placeholder="song name"></input> */}
       <button onClick={searchSong}>Search</button>
       <button>Tap to find song</button>
-      <div>{dataShow()}</div>
+      {/* <div>{dataShow()}</div> */}
     </div>
   );
 };
