abb2ff080498a23fa82fba54c787cfba7a84adc0
rotemshaked <rotemshaked8@gmail.com>
2022-01-13 14:10:49 +0200

    change height

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -2,18 +2,10 @@ import { Link } from "react-router-dom";
 import "../assets/styles.css";
 
 const Favourites = () => {
-  // const deleteSongFromFavoriteste = (e) => {
-  //   let tempArrayOfSongs = localStorage.getItem("songs");
-  //   let arrayOfSongs = JSON.parse(tempArrayOfSongs);
-  //   return arrayOfSongs.filter((song) => console.log(song, "e" + e.target));
-  // };
-
   const CardFavorite = () => {
     let tempArrayOfSongs = localStorage.getItem("songs");
     let arrayOfSongs = JSON.parse(tempArrayOfSongs);
     return arrayOfSongs.map((song) => {
-      // let songAlreadyExist = arrayOfSongs.find(song.id);
-      // if (!songAlreadyExist) {
       return (
         <div className="favoriteSongsPage">
           <div className="favoriteSong">
